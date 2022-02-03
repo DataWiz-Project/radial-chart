1a6a04297ba86edff7c111fe09c86129db732108
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-02-03 13:37:21 +0100

    style: added colors

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const prosentConverter = (threshold, shoot) => {
     const decrease = threshold - shoot
     return parseInt(decrease / shoot * 100)
   }
+
+
+// const prosentConverter = (threshold, shoot) => {
+//     return (threshold / shoot) * 100
+//   }
+//   const prosentConverter2 = (threshold, shoot) => {
+//     return (shoot / threshold) * 100
+//   }
   
   const getScale =(threshold, shoot) => {
     if(threshold > shoot){
@@ -16,21 +24,30 @@ const prosentConverter = (threshold, shoot) => {
     }
   }
 
+//   const getNewScale = (threshold, shoot) => {
+//     if(threshold < shoot){
+//         return 100 * (shoot - (threshold) / threshold)
+//     }else{
+//         return 100 * (threshold - (shoot) / shoot)
+//     }
+//   }
+
 const data = [
-    {id: 1, pb: "climate change", threshhold: -2.35, shoot: 18.95, scale: prosentConverter(-2.35, 18.95)},
-    {id: 1, pb: "Ocean acidification", threshhold: -1.59, shoot: 15.33, scale: prosentConverter(-1.59, 15.33)},
-    {id: 1, pb: "Nitrogen", threshhold: 100, shoot: 72.7, scale: prosentConverter2(100, 72.7)},
-    {id: 1, pb: "Phosphorus", threshhold: 1.1, shoot: 4, scale: prosentConverter(1.1, 4)},
-    {id: 1, pb: "Biodiversity", threshhold: 0.6, shoot: 0.45, scale: prosentConverter2(0.6, 0.45)},
+    {id: 1, pb: "climate change", threshhold: -2.35, shoot: 18.95, scale: getScale(-2.35, 18.95)},
+    {id: 1, pb: "Ocean acidification", threshhold: -1.59, shoot: 15.33, scale: getScale(-1.59, 15.33)},
+    {id: 1, pb: "Nitrogen", threshhold: 100, shoot: 72.7, scale: getScale(100, 72.7)},
+    {id: 1, pb: "Phosphorus", threshhold: 1.1, shoot: 4, scale: getScale(1.1, 4)},
+    {id: 1, pb: "Biodiversity", threshhold: 0.6, shoot: 0.45, scale: getScale(0.6, 0.45)},
   
 ];
 
-console.log(data);
+console.log(data.map(d => "Planetery boundary: " + d.pb + " Scale: " + d.scale));
+
 
 const margin = {top: 100, right: 0, bottom: 0, left: 0},
     width = 1000 - margin.left - margin.right,
     height = 1000 - margin.top - margin.bottom,
-    innerRadius = 100,
+    innerRadius = 0,
     outerRadius = Math.min(width, height) / 2;
 
 const svg = d3.select('#dataWiz')
@@ -50,6 +67,11 @@ const y = d3.scaleRadial()
     .range([innerRadius, outerRadius])
     .domain([0, d3.max(data.map(d => d.scale))])
 
+const color = d3.scaleOrdinal()
+    .range(["green", "red", "yellow", "blue", "purple"])
+
+console.log(y(data.map(d => d.scale)))
+
 const ybis = d3.scaleRadial()
     .range([innerRadius, 0])
     .domain([d3.max(data.map(d => d.scale)), 0])
@@ -58,7 +80,7 @@ svg.append('g')
     .selectAll('path')
     .data(data)
     .join('path')
-        .attr('fill', 'red')
+        .attr('fill', color)
         .attr('class', 'yo')
         .attr('d', d3.arc()
         .innerRadius(d => ybis(d.scale))
@@ -81,18 +103,18 @@ svg.append('g')
         .style('font-size', '11px')
         .attr('alignment-baseline', 'middle')
     
-svg.append('g')
-    .selectAll('path')
-    .data(data)
-    .enter()
-    .append('path')
-        .attr('fill', 'green')
-        .attr('d', d3.arc()
-        .innerRadius( function(d) { return ybis(0)})
-        .outerRadius( function(d) { return ybis(d.scale); })
-        .startAngle( function(d) { return x(d.pb);})
-        .endAngle(function(d) { return x(d.pb) + x.bandwidth();})
-        .padAngle(0.01)
-        .padRadius(innerRadius))
+// svg.append('g')
+//     .selectAll('path')
+//     .data(data)
+//     .enter()
+//     .append('path')
+//         .attr('fill', 'green')
+//         .attr('d', d3.arc()
+//         .innerRadius( function(d) { return ybis(0)})
+//         .outerRadius( function(d) { return ybis(d.scale); })
+//         .startAngle( function(d) { return x(d.pb);})
+//         .endAngle(function(d) { return x(d.pb) + x.bandwidth();})
+//         .padAngle(0.01)
+//         .padRadius(innerRadius))
 
 
